05dfd7dbb3163e758bc7961ed9be9a7a99b0f7b6
Fernandopala <fernandopalavecino18@gmail.com>
2020-10-13 00:58:15 -0300

    hola

--- resources/views/pala.blade.php
+++ resources/views/pala.blade.php
@@ -25,40 +25,6 @@
     <img alt="hola" title="auron" src="{{asset("imagenes/auron.jpg")}}" height="400px"/>
 
 
-    @extends('layouts.app')
-@section('title', 'Contact')
-@section('content')
-    <div class="container col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h2>Mensajes</h2>
-            </div>
-            @if ($mensajes->isEmpty())
-                <div>No hay Mensajes</div>
-            @else
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Título</th>
-                            <th>status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($mensajes as $mensaje)
-                            <tr>
-                                <td>{!! $mensaje->id !!}</td>
-                                <td>{!! $mensaje->titulo !!}</td>
-                                <td>{!! $mensaje->status ? 'Pendiente' : 'Respondido' !!}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @endif
-        </div>
-    </div>
-@endsection
-
    
 
 
